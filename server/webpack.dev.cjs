25d69173525b41e918dbfe4898055087532d3997
MUHAMMAD-ABDUL-RAFAY <k200180@nu.edu.pk>
2023-12-02 22:57:40 +0500

    commented out the configs files

--- server/webpack.dev.cjs
+++ server/webpack.dev.cjs
@@ -1,33 +1,33 @@
-const path = require('path')
-const nodeExternals = require('webpack-node-externals'); 
-module.exports = {
-    entry: {
-        main: './server.js'
-    },
-    output: {
-        path: path.resolve(__dirname, 'dev-build'),
-        filename: '[name].cjs',
-        publicPath: '/',
-        clean: true,
-    },
-    mode: "development",
-    target: "node",
-    externals: [nodeExternals()],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "babel-loader",
-            },
-            {
-                test: /\.html$/,
-                use: ["html-loader"],
-            },
-            {
-                test: /\.node$/,
-                use: ['file-loader'],
-              },
-        ],
-    },
-}
+// const path = require('path')
+// const nodeExternals = require('webpack-node-externals'); 
+// module.exports = {
+//     entry: {
+//         main: './server.js'
+//     },
+//     output: {
+//         path: path.resolve(__dirname, 'dev-build'),
+//         filename: '[name].cjs',
+//         publicPath: '/',
+//         clean: true,
+//     },
+//     mode: "development",
+//     target: "node",
+//     externals: [nodeExternals()],
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.js$/,
+//                 exclude: /node_modules/,
+//                 loader: "babel-loader",
+//             },
+//             {
+//                 test: /\.html$/,
+//                 use: ["html-loader"],
+//             },
+//             {
+//                 test: /\.node$/,
+//                 use: ['file-loader'],
+//               },
+//         ],
+//     },
+// }
