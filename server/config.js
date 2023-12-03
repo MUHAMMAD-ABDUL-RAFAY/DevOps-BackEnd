f9de6a7fe2ba84d2b1bedc36b2d8a3486abf9c76
MUHAMMAD-ABDUL-RAFAY <k200180@nu.edu.pk>
2023-12-04 00:15:38 +0500

    remove track of .env

--- server/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    JWT_SECRET : "<secret>",
-    EMAIL: "modesto.armstrong73@ethereal.email",
-    PASSWORD : "7P2BGPBQNDHmgs7mwB",
-}
-
-// mongodb://localhost:27017
-
-// mongodb://root:example@db:27017
